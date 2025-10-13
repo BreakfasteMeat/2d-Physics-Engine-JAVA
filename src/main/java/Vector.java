c74cfe8f041338c00a909a8983c44c055ff250a6
BreakfasteMeat <133184897+BreakfasteMeat@users.noreply.github.com>
2025-10-13 18:59:03 +0800

    init commit

---
+++ src/main/java/Vector.java
@@ -0,0 +1,69 @@
+public class Vector{
+    protected double x;
+    protected double y;
+
+    public static final Vector ZERO_VECTOR = new Vector(0, 0);
+
+
+    public Vector(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public Vector add(Vector v) {
+        return new Vector(x + v.x, y + v.y);
+    }
+    public Vector subtract(Vector v) {
+        return new Vector(x - v.x, y - v.y);
+    }
+    public Vector scaleMultiply(double scale) {
+        return new Vector(x * scale, y * scale);
+    }
+    public Vector scaleDivide(double scale) {
+        if (scale == 0){
+            throw new ArithmeticException("Cannot divide Vector with 0.");
+        }
+        return new Vector(x / scale, y / scale);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Vector) {
+            Vector v = (Vector) obj;
+            return x == v.x && y == v.y;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public static class VectorMath{
+        public static double length(Vector v1) {
+
+        }
+        public static double distance(Vector v1, Vector v2) {
+
+        }
+        public static double normalize(Vector v1) {
+
+        }
+        public static double dot(Vector v1, Vector v2) {
+
+        }
+        public static double cross(Vector v1, Vector v2) {
+
+        }
+    }
+}
---
+++ src/main/java/com/example/appdevf1/physicsengine2d/HelloController.java
@@ -0,0 +1,7 @@
+package com.example.appdevf1.physicsengine2d;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+}
